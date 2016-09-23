03889eb164876a5ac51b06ed25471f670c53c661
cloworm <chloe.echikson@gmail.com>
2016-09-23 12:05:29 -0400

    now using ng-src instead of src to remove 404 errors and removed logs

--- www/js/services.js
+++ www/js/services.js
@@ -10,7 +10,6 @@ angular.module('starter.services', [])
     all: function() {
       return $http.get('/api/posts')
       .then(function(response) {
-        console.log(response.data);
         return response.data;
       })
       .catch(function(err) {
