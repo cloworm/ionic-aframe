14bea977d671591c31d6af5da6f2a3b4e15ab34b
cloworm <chloe.echikson@gmail.com>
2016-09-24 19:19:24 -0400

    landing page is a preview of a vr experience

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,6 +34,27 @@ angular.module('starter.controllers', [])
       console.log(err);
     });
   };
+
+      $scope.options = {
+        loop: false,
+        effect: 'fade',
+        speed: 500,
+      }
+
+      $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+        // data.slider is the instance of Swiper
+        $scope.slider = data.slider;
+      });
+
+      $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+        console.log('Slide change is beginning');
+      });
+
+      $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+        // note: the indexes are 0-based
+        $scope.activeIndex = data.slider.activeIndex;
+        $scope.previousIndex = data.slider.previousIndex;
+      });
 })
 
 .controller('FriendsCtrl', function($scope, Friends, Authentication, $state) {
